d526adf95d91258f5ab35531fed76fa01b3a7101
ibnuthoriqh <ibnuthoriqh@gmail.com>
2024-11-21 23:48:18 +0700

    add start_sleep and end_sleep routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :sleeps
+  resources :sleeps do
+    collection do 
+      post 'start_sleep' => 'sleeps#start_sleep'
+      post 'end_sleep' => 'sleeps#end_sleep'
+    end
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
