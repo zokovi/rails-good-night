f8b74d4a3841e54cede2a3640037bebbe0319a15
ibnuthoriqh <ibnuthoriqh@gmail.com>
2024-11-22 20:01:44 +0700

    add follow model and associations for user following functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :sleeps
   validates :name, presence: true
 
+  has_many :followings, foreign_key: :follower_id, class_name: 'Follow', dependent: :destroy
+  has_many :followers, foreign_key: :following_id, class_name: 'Follow', dependent: :destroy
+
 end
---
+++ db/migrate/20241121190304_create_follows.rb
@@ -0,0 +1,15 @@
+class CreateFollows < ActiveRecord::Migration[7.0]
+  
+  def change
+    create_table :follows do |t|
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :following, null: false, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+
+    add_index :follows, [:follower_id, :following_id], unique: true
+  end
+end
+
+# todo: execute the migration, add 
