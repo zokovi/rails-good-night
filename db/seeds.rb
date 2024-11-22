35aa91e068efce9a6324cfbe663b82ddcde76e65
ibnuthoriqh <ibnuthoriqh@gmail.com>
2024-11-23 00:12:10 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'date'
+
+DAYS = 14
+FOURTEEN_DAYS_AGO = Date.today - DAYS
+SLEEP_START_HOUR = 22
+
+(2..10).each do |id|
+  (FOURTEEN_DAYS_AGO..Date.today).each do |date|
+    sleep_duration_minutes = rand(300..540)
+    sleep_start_time = date.to_datetime + SLEEP_START_HOUR.hours + rand(0..180).minutes
+    Sleep.create(
+      user_id: id,
+      sleep_start_time: sleep_start_time,
+      sleep_end_time: sleep_start_time + sleep_duration_minutes.minutes,
+      duration_minutes: sleep_duration_minutes
+    )
+  end
+end
+
